0874c47e2d3c4a81fab210a44db244694cfeb1b9
RyotaBannai <ryotabannai0528@gmail.com>
2019-01-20 15:19:05 -0800

    first commit

---
+++ Intro_R.r
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+mpg
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, color = class))
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
