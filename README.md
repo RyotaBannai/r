cc50a2f9419a51fff0b10f4b7efee04e0113fce5
RyotaBannai <ryotala0528@gmail.com>
2023-06-16 02:24:50 +0900

    refactor: use walk instead of loop

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rdev
 - This repository includs following codes:
-  - 競プロコード in R 
+  - 競プロコード in R
   - R 勉強のためのランダムコード
   - 統計的な作業の確認、書き捨てコード
--- playground/copy_dir.r
+++ playground/copy_dir.r
@@ -41,28 +41,27 @@ copy_dir <- function(from, to) {
 # applying tail iteratively through the strsplit list, taking the 1st element of the tail-end of each vector.
 # The 1 is telling tail how many elements to take. The default is six, which is what you were probably getting
 
-target_dir_pattern <- "amazing_folder_name" # 探索したいフォルダ名. 正規表現でもok
-dirs <- list.files(path, recursive = TRUE, include.dirs = TRUE, pattern = target_dir_pattern) |>
+target_dir_pattern <- "spark" # 探索したいフォルダ名. 正規表現でもok
+list.files(path, recursive = TRUE, include.dirs = TRUE, pattern = target_dir_pattern) |>
     map(trimws) |>
     map(\(x) c(sapply(strsplit(x, "/"), tail, 1), x)) |>
-    unique()
+    unique() |>
+    walk(\(dir) {
+        copy_to <- sprintf("%s/%s", result_path, dir[1])
+        copy_from <- sprintf("%s/%s", path, dir[2])
+        # なければ作成
+        if (!dir.exists(copy_to)) {
+            dir.create(copy_to, recursive = TRUE)
+        }
 
-for (dir in dirs) {
-    copy_to <- sprintf("%s/%s", result_path, dir[1])
-    copy_from <- sprintf("%s/%s", path, dir[2])
-    # なければ作成
-    if (!dir.exists(copy_to)) {
-        dir.create(copy_to, recursive = TRUE)
-    }
-
-    # フォルダ配下のファイルを全てcopy
-    for (y in list.files(copy_from, recursive = TRUE, include.dirs = TRUE)) {
-        from <- sprintf("%s/%s", copy_from, y)
-        # debug
-        # cat(sprintf("%s\n", from))
-        to <- sprintf("%s/%s", copy_to, y)
-        file.copy(from, to)
-    }
-}
+        # フォルダ配下の全ファイルをcopy
+        # ここで include.dirs=FALSE にしてもフォルダ名を拾ってくるのはなぜなんだろう.
+        list.files(copy_from, include.dirs = FALSE) |> walk(\(y) {
+            z <- sprintf("%s/%s", copy_from, y) # 絶対path
+            if (file_test("-f", z)) {
+                file.copy(from = z, to = sprintf("%s/%s", copy_to, y))
+            }
+        })
+    })
 
 cat("done.")
